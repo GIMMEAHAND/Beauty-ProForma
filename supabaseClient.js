a5096031df7c008fac245becb437b8f91a68334d
Beauty MetriXs <93333560+GIMMEAHAND@users.noreply.github.com>
2026-04-08 11:11:40 -0400

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,5 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-client'
 
+// These names must match EXACTLY what you typed into Vercel
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
